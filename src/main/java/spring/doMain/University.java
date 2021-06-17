746044c9f2434154743086e4abdcc984e7c42bd5
RossSihovsk <rsihovskiy@gmail.com>
2021-06-17 21:53:21 +0300

    Initial commit

---
+++ src/main/java/spring/doMain/University.java
@@ -0,0 +1,80 @@
+package spring.doMain;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class University {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private int countOfInstitutes;
+    @Column
+    private int countOfStudents;
+    @Column
+    private String address;
+
+    public University(String name, int countOfInstitutes, int countOfStudents, String address) {
+        this.name = name;
+        this.countOfInstitutes = countOfInstitutes;
+        this.countOfStudents = countOfStudents;
+        this.address = address;
+    }
+
+    public University() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCountOfInstitutes() {
+        return countOfInstitutes;
+    }
+
+    public void setCountOfInstitutes(int countOfInstitutes) {
+        this.countOfInstitutes = countOfInstitutes;
+    }
+
+    public int getCountOfStudents() {
+        return countOfStudents;
+    }
+
+    public void setCountOfStudents(int countOfStudents) {
+        this.countOfStudents = countOfStudents;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", countOfInstitutes=" + countOfInstitutes +
+                ", countOfStudents=" + countOfStudents +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
